b6adae757e39c714d81bd6ea2a22e22c506a62e0
philanderadriaan <philander90@msn.com>
2014-03-28 13:39:22 -0700

    Added path classes.

--- src/utility/Paths.java
+++ src/utility/Paths.java
@@ -1,21 +1,37 @@
 /**
  * 
  */
+
 package utility;
+
+import io.CSV;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import enums.PathEnum;
 
 /**
  * @author padriaan
- *
+ * 
  */
 public class Paths
 {
-
   /**
    * 
    */
+  private static Map<PathEnum, String> my_paths = getPaths();
+
   public Paths()
   {
-    // TODO Auto-generated constructor stub
   }
 
+  private static Map<PathEnum, String> getPaths() throws IOException
+  {
+    final Map<PathEnum, String> paths = new HashMap<PathEnum, String>();
+    final List<List<String>> locations = CSV.read("Paths.txt");
+    return paths;
+  }
 }
