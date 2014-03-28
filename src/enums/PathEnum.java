122c2281c309213048a3d1f777de634c62d071d0
philanderadriaan <philander90@msn.com>
2014-03-28 16:00:46 -0700

    Updated documentation.

--- src/enums/PathEnum.java
+++ src/enums/PathEnum.java
@@ -1,13 +1,25 @@
-/**
- * 
- */
 package enums;
 
 /**
  * @author padriaan
- *
+ * @version 1
  */
 public enum PathEnum
 {
-  TABLES, REPORTS, HISTORIES, ICONS;
+  /**
+   * Folder containing all the data tables.
+   */
+  TABLES, 
+  /**
+   * Folder containing all the reports generated at the end of the month.
+   */
+  REPORTS,
+  /**
+   * Folder containing the location histories.
+   */
+  HISTORIES,
+  /**
+   * Folder containing icons.
+   */
+  ICONS;
 }
--- src/utility/Paths.java
+++ src/utility/Paths.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 
 package utility;
 
@@ -14,24 +11,49 @@ import java.util.Map;
 import enums.PathEnum;
 
 /**
- * @author padriaan
+ * This class manages the header.
  * 
+ * @author padriaan
+ * @version 1
  */
 public class Paths
 {
   /**
-   * 
+   * Map to store the paths.
    */
-  private static Map<PathEnum, String> my_paths = getPaths();
+  private static Map<PathEnum, String> my_path_map = getPaths();
 
-  public Paths()
+  /**
+   * Prevents instantiation.
+   */
+  private Paths()
   {
   }
 
-  private static Map<PathEnum, String> getPaths() throws IOException
+  private static Map<PathEnum, String> getPaths()
   {
-    final Map<PathEnum, String> paths = new HashMap<PathEnum, String>();
-    final List<List<String>> locations = CSV.read("Paths.txt");
-    return paths;
+    final Map<PathEnum, String> path_map = new HashMap<PathEnum, String>();
+    try
+    {
+      final List<List<String>> path_list = CSV.read("Paths.csv");
+      for (List<String> i : path_list)
+      {
+        final String key_string = i.get(0);
+        final PathEnum key = PathEnum.valueOf(key_string);
+        final String value = i.get(1);
+        path_map.put(key, value);
+      }
+    }
+    catch (final IOException e)
+    {
+      e.printStackTrace();
+    }
+    return path_map;
+  }
+
+  public static String getPath(final PathEnum the_path_enum)
+  {
+    final String path = my_path_map.get(the_path_enum);
+    return path;
   }
 }
